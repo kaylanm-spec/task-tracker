98d2157159ed4e8d9a8f4582a05b1d7b37a45f30
Kaylan Moore <your-email@example.com>
2026-07-09 15:23:34 -0400

    Add initial project files

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My First Project</title>
+</head>
+
+<body>
+
+<h1>Welcome to My First Website</h1>
+
+<p>I built this using Laragon.</p>
+
+<link rel="stylesheet" href="style.css">
+
+</body>
+
+</html>
